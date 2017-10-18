b1867243100bbf402b91cfb4650e00688d2ab6e3
Golam Rabbani <knight3rrantt@gmail.com>
2017-10-18 22:49:19 +0600

    color fixes

--- components/Question.js
+++ components/Question.js
@@ -8,7 +8,7 @@ import {
 import randomColor from 'randomcolor';
 
 
-let colors= ['#27CB7E','#351B9B','#E22892','#1CABDA']
+let colors= ['#27CB7E','#351B9B','#E22892','#27CB7E']
 let color;
 
 export default Question = (props) => {
